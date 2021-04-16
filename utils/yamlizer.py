c6553e97620a7ac791a2a12327bbee6952838132
Atharv Joshi <atharvjoshi@ymail.com>
2021-04-16 16:48:29 +0800

    Make sequences follow flow style
    Add representer that enforces flow style for all python sequences.

--- utils/yamlizer.py
+++ utils/yamlizer.py
@@ -4,7 +4,7 @@ Yamlable inherits YAMLOBject and defines methods to save and load subclasses
 from yaml files. Subclasses can define a dictionary of parameters that gets
 written to and read from yaml file.
 """
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
 import yaml
 
 YAML_TAG_PREFIX = u'!'
@@ -16,13 +16,18 @@ def sci_not_representer(dumper, value):
                         else str(value))
     return dumper.represent_scalar(yaml_float_tag, value_in_sci_not)
 
-class AbstractYamlable(ABCMeta):
+# lists must be always represented in flow style, not block style
+def sequence_representer(dumper, value):
+    yaml_seq_tag = u'tag:yaml.org,2002:seq'
+    return dumper.represent_sequence(yaml_seq_tag, value, flow_style=True)
+
+class YamlableMetaclass(type):
     """
     Defines yaml tag, loader, dumper for all inheriting classes. Registration
     of class to yaml is done by inheriting from this metaclass.
     """
     def __init__(cls, name, bases, kwds):
-        super(AbstractYamlable, cls).__init__(name, bases, kwds)
+        super(YamlableMetaclass, cls).__init__(name, bases, kwds)
         # set a consistent format for subclass yaml tags
         cls.yaml_tag = YAML_TAG_PREFIX + name
         # register loader
@@ -31,8 +36,10 @@ class AbstractYamlable(ABCMeta):
         yaml.SafeDumper.add_representer(cls, cls.to_yaml)
         # custom dumper for representing float in scientific notation
         yaml.SafeDumper.add_representer(float, sci_not_representer)
+        # custom dumper for always representing tuples and lists in flow style
+        yaml.SafeDumper.add_representer(list, sequence_representer)
 
-class Yamlable(metaclass = AbstractYamlable):
+class Yamlable(metaclass = YamlableMetaclass):
     """
     All Yamlables are abstract, need to define create yaml map method.
     """
