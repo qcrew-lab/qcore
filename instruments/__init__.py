03c34cb5e84ca6ce7e43875cce7385ce3171405a
Atharv Joshi <atharvjoshi@ymail.com>
2021-04-16 16:48:59 +0800

    Update __init__.py

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -4,3 +4,4 @@ __init__.py
 from .vaunix.labbrick import LabBrick
 from .signal_hound.sa124 import Sa124
 from .instrument import (Instrument, MetaInstrument, PhysicalInstrument)
+from .meta.quantum_element import QuantumElement, QuantumDevice
