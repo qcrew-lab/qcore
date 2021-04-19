bb319c498df4ac821d2505060f9ceb1b763ae687
qcrew-lab <general.qcrew@gmail.com>
2021-04-19 19:44:16 +0800

    Update configuration.py

--- instruments/quantum_machines/configuration.py
+++ instruments/quantum_machines/configuration.py
@@ -32,8 +32,8 @@ def IQ_imbalance(g, phi):
 long_readout_len = 1000
 readout_len = 400
 
-qubit_IF = int(-50e6)
-rr_IF = int(-47.25e6) #int(-50e6)
+qubit_IF = int(-54e6)
+rr_IF = int(-47.5e6) #int(-50e6)
 
 qubit_LO = int(4.165e9)
 rr_LO = int(8.7571e9)
