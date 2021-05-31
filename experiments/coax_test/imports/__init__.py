ca6e12f16636e71c3579b8917a77602c51c2311a
qcrew-lab <general.qcrew@gmail.com>
2021-05-31 16:55:52 +0800

    Make path generation absolute

--- experiments/coax_test/imports/__init__.py
+++ experiments/coax_test/imports/__init__.py
@@ -17,5 +17,5 @@ from datetime import datetime, date
 from pathlib import Path
 
 DAILY_SUBFOLDER = str(date.today())
-DATA_FOLDER_PATH = Path.cwd() / "data" / STAGE_NAME / DAILY_SUBFOLDER
+DATA_FOLDER_PATH = Path().resolve() / "data" / STAGE_NAME / DAILY_SUBFOLDER
 DATA_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
