cfadb019cfda22f4a548205be3d501372d8d3728
qcrew-lab <general.qcrew@gmail.com>
2021-06-02 20:08:06 +0800

    Make FakeLivePlotter class

--- codebase/analysis/plot.py
+++ codebase/analysis/plot.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+from IPython import display
 
 from qcrew.codebase.analysis import fit
 
@@ -19,3 +20,20 @@ def plot_fit(xs, ys, axis, y_errs=None, fit_func="sine"):
     axis.plot(xs, fit_ys)
 
     return params
+
+class FakeLivePlotter:
+    def __init__(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        self.hdisplay = display.display("", display_id=True)
+
+    def plot(self, x, y, N: int = None, fit_func: str = None):  # 2D plots only
+        """ """
+        self.ax.clear()
+        self.ax.plot(x, y)  # plot data
+        if fit_func is not None:
+            plot_fit(x, y, self.ax, fit_func=fit_func)  # plot fit
+        if N is not None:
+            self.ax.set_title(f"Rolling average after {N} reps")
+
+        self.hdisplay.update(self.fig)  # update figure
