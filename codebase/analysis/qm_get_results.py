91892a60df14fcc9ece33e36ab5da83350e2676a
qcrew-lab <general.qcrew@gmail.com>
2021-06-07 13:00:09 +0800

    Dump code for getting stdev

--- codebase/analysis/qm_get_results.py
+++ codebase/analysis/qm_get_results.py
@@ -26,4 +26,25 @@ def update_results(raw_data, N, result_handles, handle_tags):
         raw_data[tag] = np.array(list(raw_data[tag]) + list(partial_results))
 
     return raw_data
-        
+
+
+""" Dumping the code for getting stdev of data here
+# get stdev of data over N to check if we are averaging correctly
+result_handles.wait_for_all_values()
+I_raw = result_handles.I.fetch_all(flat_struct = True)
+Q_raw = result_handles.Q.fetch_all(flat_struct = True)
+amps_raw = np.abs((I_raw + 1j * Q_raw)).T
+stdevs_raw = np.zeros((reps, len(qubit_a_list)))
+
+I_avg = result_handles.I_avg.fetch_all(flat_struct = True)
+Q_avg = result_handles.Q_avg.fetch_all(flat_struct = True)
+amps_avg = np.abs((I_avg + 1j * Q_avg)).T
+stdevs_avg = np.zeros((reps, len(qubit_a_list)))
+
+for i in range(1, reps + 1):
+    stdevs_raw[i - 1] = np.std(amps_raw[:,:i], axis=1)
+    stdevs_avg[i - 1] = np.std(amps_avg[:,:i], axis=1)
+
+stdevs_raw_avg = np.mean(stdevs_raw, axis = 1)
+stdevs_avg_avg = np.mean(stdevs_avg, axis = 1)
+"""
