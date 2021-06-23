a121fb689304a804a36b3ae4a59cce46c15556b8
qcrew-lab <general.qcrew@gmail.com>
2021-06-23 16:14:55 +0800

    Create drag test package to send to QM

---
+++ experiments/coax_test/measurements/unfinished/drag_sim/configuration.py
@@ -0,0 +1,100 @@
+""" QM config for drag pulse simulation """
+import numpy as np
+import matplotlib.pyplot as plt
+
+DRAG_COEFFICIENT = 0.5  # used for scaling the DRAG Q waveform
+
+########################     WAVEFORM SAMPLE GENERATOR FNS     #########################
+
+BASE_AMP = 0.25
+
+
+def drag_wfs(ampx: float, drag: float, sigma: float, chop: int) -> tuple[np.ndarray]:
+    ts = np.linspace(-chop / 2 * sigma, chop / 2 * sigma, chop * sigma)
+    i_wf = BASE_AMP * ampx * np.exp(-(ts ** 2) / (2.0 * sigma ** 2))
+    q_wf = drag * (np.exp(0.5) / sigma) * -ts * i_wf
+    return i_wf, q_wf
+
+
+#############################     TOP LEVEL CONSTANTS     ##############################
+
+QUBIT_LO_FREQ = 5e9
+QUBIT_INT_FREQ = -50e6
+
+AMPX, DRAG = 1.0, 1.0
+SIGMA, CHOP = 175, 4
+DRAG1_WF_SAMPLES_I, DRAG1_WF_SAMPLES_Q = drag_wfs(AMPX, DRAG, SIGMA, CHOP)
+DRAG_PULSE_LEN = SIGMA * CHOP
+
+plt.plot(DRAG1_WF_SAMPLES_I)
+plt.plot(DRAG1_WF_SAMPLES_Q)
+plt.plot(DRAG1_WF_SAMPLES_Q * DRAG_COEFFICIENT)
+
+################################      QM CONFIG      ###################################
+
+qm_config = {
+    "version": 1,
+    "controllers": {
+        "con1": {
+            "type": "opx1",
+            "analog_outputs": {
+                1: {"offset": 0.0},
+                2: {"offset": 0.0},
+            },
+        },
+    },
+    "elements": {
+        "qubit": {
+            "mixInputs": {
+                "I": ("con1", 1),
+                "Q": ("con1", 2),
+                "lo_frequency": int(QUBIT_LO_FREQ),
+                "mixer": "mixer_qubit",
+            },
+            "intermediate_frequency": int(QUBIT_INT_FREQ),
+            "operations": {
+                "drag1": "drag1",
+                "drag2": "drag2",
+            },
+        },
+    },
+    "pulses": {
+        "drag1": {
+            "operation": "control",
+            "length": DRAG_PULSE_LEN,
+            "waveforms": {"I": "gauss_wf", "Q": "dgauss_wf_1"},
+        },
+        "drag2": {
+            "operation": "control",
+            "length": DRAG_PULSE_LEN,
+            "waveforms": {"I": "gauss_wf", "Q": "dgauss_wf_2"},
+        },
+    },
+    "waveforms": {
+        "zero_wf": {
+            "type": "constant",
+            "sample": 0.0,
+        },
+        "gauss_wf": {
+            "type": "arbitrary",
+            "samples": DRAG1_WF_SAMPLES_I,
+        },
+        "dgauss_wf_1": {
+            "type": "arbitrary",
+            "samples": DRAG1_WF_SAMPLES_Q,  # will be scaled in QUA loop
+        },
+        "dgauss_wf_2": {
+            "type": "arbitrary",
+            "samples": DRAG1_WF_SAMPLES_Q * DRAG_COEFFICIENT,  # scale in QM config
+        },
+    },
+    "mixers": {
+        "mixer_qubit": [
+            {
+                "intermediate_frequency": int(QUBIT_INT_FREQ),
+                "lo_frequency": int(QUBIT_LO_FREQ),
+                "correction": (1.0, 0.0, 0.0, 1.0),
+            },
+        ],
+    },
+}
---
+++ experiments/coax_test/measurements/unfinished/drag_sim/drag_sim.py
@@ -0,0 +1,44 @@
+""" Check drag pulse waveforms with the QM simulator """
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm.qua import program, play, amp
+from qm import SimulationConfig
+from .configuration import qm_config, DRAG_COEFFICIENT, DRAG_PULSE_LEN
+from .helpers import plot_waveforms, get_processed_samples
+
+qmm = QuantumMachinesManager()
+
+################################     QUA PROGRAMS     ##################################
+
+# in "test1", we scale "drag1" Q waveform in qua loop
+with program() as test1:
+    play("drag1" * amp(1.0, 0.0, 0.0, DRAG_COEFFICIENT), "qubit")
+
+# in "test2", we play "drag2", whose Q waveform is scaled in the QM config
+with program() as test2:
+    play("drag2", "qubit")
+
+##############################     SIMULATION SETUP     ################################
+
+DELAY = 100
+sim_duration = int(DELAY + (DRAG_PULSE_LEN // 4))
+sim_config = SimulationConfig(duration=sim_duration, include_analog_waveforms=True)
+
+###############################     RUN SIMULATIONS     ################################
+
+job1 = qmm.simulate(qm_config, test1, sim_config)
+samples1 = job1.get_simulated_samples()
+job2 = qmm.simulate(qm_config, test2, sim_config)
+samples2 = job2.get_simulated_samples()
+
+###############################     POST-PROCESSING     ################################
+
+PADDING = 100  # plot timestamps before and after pulse is played
+
+waveforms = {
+    "i_1": get_processed_samples(samples1, job1, "1", PADDING),
+    "q_1": get_processed_samples(samples1, job1, "2", PADDING),
+    "i_2": get_processed_samples(samples2, job2, "1", PADDING),
+    "q_2": get_processed_samples(samples2, job2, "2", PADDING),
+}  # these waveforms have been sliced so that they now share the same time axis
+
+plot_waveforms(waveforms)
