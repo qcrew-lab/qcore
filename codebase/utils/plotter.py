690730de48bb6bb268ed3f7bd8546aba47dcdda8
qcrew-lab <general.qcrew@gmail.com>
2021-06-24 13:05:57 +0800

    Write simple plotter helper

--- codebase/utils/plotter.py
+++ codebase/utils/plotter.py
@@ -1,20 +1,63 @@
 """ Qcrew plotter v1.0 """
 
+import time
+
 import matplotlib.pyplot as plt
+from IPython import display
+
 
 class Plotter:
-    def __init__(self):
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(1, 1, 1)
-        self.hdisplay = display.display("", display_id=True)
 
-    def plot(self, x, y, N: int = None, fit_func: str = None):  # 2D plots only
-        """ """
-        self.ax.clear()
-        self.ax.plot(x, y)  # plot data
-        if fit_func is not None:
-            plot_fit(x, y, self.ax, fit_func=fit_func)  # plot fit
-        if N is not None:
-            self.ax.set_title(f"Rolling average after {N} reps")
+    live_plot_refresh_delay: float = 0.1
 
-        self.hdisplay.update(self.fig)  # update figure
+    """ Single axis x-y plotter. Supports line, scatter, and errorbar plot. Provides a rudimentary live plotting routine. """
+    def __init__(self, title: str, xlabel: str, ylabel: str = "Signal (A.U.)"):
+        plt.rcParams["figure.figsize"] = (12, 8)  # adjust figure size
+
+        self.figtitle: str = title
+        self.xlabel: str = xlabel
+        self.ylabel: str = ylabel
+
+    def live_plot(self, x, y, n, fit = None, err = None):
+        """" If `live_plot(data)` is called in an IPython terminal context, the axis is refreshed and plotted with the new data using IPython `display` tools. """
+        plt.cla()
+
+        if err is not None:
+            self.plot_errorbar(x, y, err, "data")
+        else:
+            self.plot_scatter(x, y)
+
+        if fit is not None:
+            self.plot_line(x, fit, "fit")
+
+        plt.title(self.figtitle + f": {n} reps")
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.legend()
+
+        display.display(plt.gcf())  # plot latest batch
+        display.clear_output(wait=True)  # clear plot when new plot is available
+        time.sleep(self.live_plot_refresh_delay)  # to prevent ultra-fast live plotting
+
+    def plot_errorbar(self, x, y, err, label):
+        plt.errorbar(
+            x,
+            y,
+            yerr=err,
+            label=label,
+            ls="none",
+            lw=1,
+            ecolor="black",
+            marker="o",
+            ms=4,
+            mfc="black",
+            mec="black",
+            capsize=3,
+            fillstyle="none",
+        )
+
+    def plot_scatter(self, x, y):
+        plt.scatter(x, y, s=4, c="black", marker="o", fillstyle="none")
+
+    def plot_line(self, x, y, label):
+        plt.plot(x, y, color="m", lw=2, label=label)
