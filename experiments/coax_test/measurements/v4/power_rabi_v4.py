45a0cef331d1849141ce601a3063943007d5963e
qcrew-lab <general.qcrew@gmail.com>
2021-06-24 13:07:00 +0800

    Finish writing and preliminary testing of v4 script
    Outstanding bug: std_err not correctly calculated

--- experiments/coax_test/measurements/v4/power_rabi_v4.py
+++ experiments/coax_test/measurements/v4/power_rabi_v4.py
@@ -1,4 +1,4 @@
-""" Power Rabi measurement script v4 """
+""" Power Rabi measurement script v4, with notes on data saving for Yifan"""
 from qcrew.experiments.coax_test.imports import *
 
 reload(cfg), reload(stg)  # reload before each measurement run
@@ -10,15 +10,15 @@ meas_name = "power_rabi"  # appended to saved datafile name
 filename_suffix = meas_name + ""  # replace "" to customise filename
 
 datatags: tuple = ("I", "Q", "Y_RAW", "Y_AVG")  # to identify datasets
-# NOTE these datatags will be used by stream processing, Fetcher, and Datasaver
+# NOTE these datatags are meant to standardize the names of the datasets for stream processing, fetching, and saving. Please use them to create group / dataset keys in the hdf5 file.
 
 ######################        SET MEASUREMENT PARAMETERS        ########################
 
 mdata = {  # metadata dictionary
-    "reps": 40000,  # number of sweep repetitions
+    "reps": 10000,  # number of sweep repetitions
     "wait": 50000,  # delay between reps in ns, an integer multiple of 4 >= 16
-    "a_start": -1.6,  # amplitude sweep range is set by a_start, a_stop, and a_step
-    "a_stop": 1.6,
+    "a_start": -2.0,  # amplitude sweep range is set by a_start, a_stop, and a_step
+    "a_stop": 2.0,
     "a_step": 0.05,
     "qubit_op": "gaussian",  # qubit pulse name as defined in the config
     "rr_op": "readout",  # readout pulse name
@@ -76,16 +76,18 @@ with program() as power_rabi:
 
 job = stg.qm.execute(power_rabi)
 
-######################        SAVE MEASUREMENT RUN METADATA       ######################
-
-
-
 #############################        INVOKE HELPERS        #############################
 
 datasaver = Datasaver(sample_name=STAGE_NAME, filename_suffix=filename_suffix)
+# NOTE I have written a simple __init__ method for the Datasaver class which generates folder and file paths and opens a new HDF5 file associated with this measurement run. You can add other methods e.g. save_metadata() and save_data() to this class.
+
 fetcher = Fetcher(handle=job.result_handles, tags=datatags)
 plotter = Plotter(title=meas_name, xlabel="Amplitude scale factor")
-stats = tuple()  # tuple holding vars needed to calculate running std err
+stats = tuple()  # tuple holding vars needed to calculate running std err in single pass
+
+######################        SAVE MEASUREMENT RUN METADATA       ######################
+
+# NOTE here, we should save the "mdata" dictionary to the HDF5 file. The idiomatic way of saving metadata in the HDF5 data model is via "Attributes". See https://docs.h5py.org/en/stable/high/attr.html.
 
 #############################        POST-PROCESSING        ############################
 
@@ -93,143 +95,41 @@ while fetcher.count != mdata["reps"]:  # while all results have not been fetched
 
     ######################            FETCH PARTIAL RESULTS         ####################
 
+    prev_count = fetcher.count  # save previous result count before fetch() updates it
     partial_data = fetcher.fetch()  # return dict with (k, v) = (datatag, partial data)
-    current_count = fetcher.count  # get current number of fetched results
+    curr_count = fetcher.count  # get current number of fetched results
 
     ######################            SAVE FETCHED RESULTS         #####################
 
-
+    # NOTE here, we should "live" save the latest batch of available results that "fetcher" has fetched. Fetch() returns "partial_data", where key = "datatag" and value = numpy array holding partial results from batch number "prev_count" to "curr_count". You can use "prev_count" and "curr_count" together with "mdata["sweep_len"]" and "mdata["reps"]" to track partial result array shapes. Note that the Datasaver class I've written has already opened the HDF5 file in its __init__ method, so there's no need for a with() statement, as long as the Datasaver closes the file at the end of this script. Please feel free to ask me any questions about this step, because its the most complex and critical step wrt data saving.
 
     ####################            PROCESS AVAILABLE RESULTS         ##################
 
-    y_avg = np.sqrt(partial_data[datatags[3]][-1])  # get latest y_avg
-    y_raw = np.sqrt(partial_data[datatags[2]])  # get latest batch of y_raw
+    y_raw = np.sqrt(partial_data[datatags[2]])  # latest batch of y_raw
+    y_avg = np.sqrt(partial_data[datatags[3]])  # latest batch of y_avg
 
-    if stats:  # track running stats so that y_std_err is calculated in a single pass
-        stats = get_std_err(y_raw, current_count, *stats)
+    if stats:  # stats = (y_std_err, running average, running variance * (count-1))
+        stats = get_std_err(y_raw, y_avg, curr_count, *stats)
     else:
-        stats = get_std_err(y_raw, current_count)
-    y_std_err = stats[0]  # get latest y_std_err
+        stats = get_std_err(y_raw, y_avg, curr_count)
 
-    # TODO get fit
+    fit_params = fit.do_fit(mdata["fit_fn"], xs, y_avg[-1])  # get fit parameters
+    y_fit = fit.eval_fit(mdata["fit_fn"], fit_params, xs)  # get fit values
 
     ###################            LIVE PLOT AVAILABLE RESULTS         #################
 
-    plotter.live_plot(x=xs, y=y_avg, fit=None, err=y_std_err, n=current_count)
+    plotter.live_plot(x=xs, y=y_avg[-1], n=curr_count, fit=y_fit, err=stats[0])
 
-# SAVE PLOT TO HDF5
-# DATASAVER HOUSEKEEPING i.e flush, close, print paths
-# PRINT JOB EXECUTION REPORT AND MEASUREMENT EXECUTION TIME
+###############################         SAVE PLOT         ##############################
 
+# NOTE (OPTIONAL) here, we can save the final plot. If we want to do that, I suggest we save it in the same HDF5 file (HDF5 can save images too), so that each measurement run is associated with a single saved file.
 
-plt.rcParams["figure.figsize"] = (12, 8)  # adjust figure size
+#########################         DATASAVER WIND DOWN         ##########################
 
-while handle.is_processing():  # while the measurement is running
+# NOTE use this block to do any final housekeeping for the Datasaver, such as flush, file close e.t.c.
 
-    # calculate std error from raw data
-    signal_sq_raw = handle.signal_sq_raw.fetch_all(flat_struct=True)
-    signal_raw = np.sqrt(signal_sq_raw)
-    mean_std_error = scipy.stats.sem(signal_raw, axis=0)
+###############################          fin           #################################
 
-    ###################            LIVE PLOT PARTIAL RESULTS         ###################
-
-    plt.cla()  # refresh
-    plt.errorbar(
-        ampxs,
-        signal_avg,
-        yerr=mean_std_error,
-        ls="none",
-        lw=1,
-        ecolor="black",
-        marker="o",
-        ms=4,
-        mfc="black",
-        mec="black",
-        capsize=3,
-        fillstyle="none",
-    )
-    plt.title(f"Power Rabi: {num_results} reps")
-    plt.xlabel("Amplitude scale factor")
-    plt.ylabel("Signal amplitude (A.U.)")
-    display.display(plt.gcf())  # display latest batch
-    display.clear_output(wait=True)  # clear latest batch after new batch is available
-    time.sleep(0.5)  # add a short delay before next plot refresh
-
-###########################          FETCH FINAL RESULTS       #########################
-
-print(f"{meas_name} completed, fetching results...")  # log message
-
-i_avg = handle.i_avg.fetch_all(flat_struct=True)  # fetch final average I and Q values
-q_avg = handle.q_avg.fetch_all(flat_struct=True)
-signal_avg = np.abs(i_avg + 1j * q_avg)  # calculate final average signal
-
-i_raw = handle.i_raw.fetch_all(flat_struct=True)  # fetch all raw I & Q values
-q_raw = handle.q_raw.fetch_all(flat_struct=True)
-signal_raw = np.abs(i_raw + 1j * q_raw)  # calculate final raw signal
-mean_std_error = scipy.stats.sem(signal_raw, axis=0)  # for plotting errorbars
-
-###############################          FIT RESULTS       #############################
-
-fit_params = fit.do_fit(mdata["fit_fn"], ampxs, signal_avg)  # get fit parameters
-ys_fit = fit.eval_fit(mdata["fit_fn"], fit_params, ampxs)  # get fit values
-for name, value in fit_params.valuesdict().items():  # save fit parameters to metadata
-    mdata[f"fit_param_{name}"] = value
-
-##############################          PLOT RESULTS       #############################
-
-plt.cla()  # refresh plot
-plt.errorbar(  # plot final results as a scatter plot with errorbars
-    ampxs,
-    signal_avg,
-    yerr=mean_std_error,
-    ls="none",
-    lw=1,
-    ecolor="black",
-    marker="o",
-    ms=4,
-    mfc="black",
-    mec="black",
-    capsize=3,
-    label="data",
-    fillstyle="none",
-)
-plt.plot(ampxs, ys_fit, color="m", lw=2, label="fit")  # plot fitted values
-plt.title(f"Power Rabi: {mdata['reps']} reps")
-plt.xlabel("Amplitude scale factor")
-plt.ylabel("Signal amplitude (A.U.)")
-plt.legend()
-
-###############################         GENERATE PATHS     #############################
-
-folderpath = Path.cwd() / f"data/{STAGE_NAME}/{str(date.today())}"
-folderpath.mkdir(parents=True, exist_ok=True)  # create daily subfolder if none exists
-filename = f"{datetime.now().strftime('%H-%M-%S')}_{meas_name}"
-filepath = folderpath / filename
-
-#################################          SAVE DATA       #############################
-
-print(f"Fetched {meas_name} results, saving data...")  # log message
-
-datapath_str = str(filepath) + ".hdf5"
-with h5py.File(datapath_str, "a") as file:  # save to hdf5 file
-    file.create_dataset("ampxs", data=ampxs)  # save frequency sweep values
-    file.create_dataset("i_raw", data=i_raw)  # save all raw I values
-    file.create_dataset("q_raw", data=q_raw)  # save all raw Q values
-    file.create_dataset("i_avg", data=i_raw)  # save final average I values
-    file.create_dataset("q_avg", data=i_raw)  # save final average Q values
-
-    for name, value in mdata.items():  # save metadata as file attributes
-        file.attrs[name] = value
-
-print(f"Data saved at {datapath_str}")  # log message
-
-#################################          SAVE PLOT       #############################
-
-imgpath_str = str(filepath) + ".png"
-plt.savefig(imgpath_str, format="png", dpi=600)
-print(f"Plot saved at {imgpath_str}")
-
-####################################          fin        ###############################
-
+elapsed_time = time.perf_counter() - start_time
+print(f"Measurement done! Execution time: {str(timedelta(seconds=elapsed_time))}")
 print(job.execution_report())
-print("Here's the final plot :-) \n")
