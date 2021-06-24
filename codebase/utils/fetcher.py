870612c134910f26f4558ea708c48e30187f54dc
qcrew-lab <general.qcrew@gmail.com>
2021-06-24 17:06:05 +0800

    Add check for no new available results

--- codebase/utils/fetcher.py
+++ codebase/utils/fetcher.py
@@ -9,7 +9,6 @@ class Fetcher:
 
     def __init__(self, handle: JobResults, tags: tuple[str]) -> None:
         self.count: int = 0  # number of results fetched so far
-
         self.handle: JobResults = handle
         self.tags: tuple[str] = tags
         for tag in tags:  # wait for at least 2 results to be processed
@@ -19,6 +18,8 @@ class Fetcher:
     def fetch(self) -> dict[str, np.ndarray]:
         """ Fetch next batch of available results for all tags in result handle """
         count = min(len(self.handle.get(tag)) for tag in self.tags)
+        if count == self.count:  # no new results to fetch
+            return dict()
         slc = slice(self.count, count)
         self.count = count
         return {t: self.handle.get(t).fetch(slc, flat_struct=True) for t in self.tags}
