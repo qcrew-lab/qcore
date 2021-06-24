eb883e0f67a274444dd5b76135a441f3c9a6c3e1
qcrew-lab <general.qcrew@gmail.com>
2021-06-24 13:06:13 +0800

    Implement running std error algo

--- codebase/utils/statistician.py
+++ codebase/utils/statistician.py
@@ -1,17 +1,15 @@
 """ Utility to calculate statistics in a single pass """
+import numpy as np
 
 # Python implementation of Welford's online algorithm
 # Inspired by http://www.johndcook.com/standard_deviation.html
-def get_std_err(xs, n, old_std_err=None, old_m=None, old_s=None):
-    if old_std_err is None:
-        new_m, new_s = _get_m_s(xs[1:], xs.shape[0] - 1, xs[0], 0)
+def get_std_err(xs, ms, n, std_err=None, m=None, s=None):
+    if std_err is None:
+        old_m, old_s = ms[0], np.zeros(ms.shape[1])
+        xs, ms = xs[1:], ms[1:]
     else:
-        new_m, new_s = _get_m_s(xs, xs.shape[0], old_m, old_s)
-    new_std_err = new_s / (n * (n - 1))
-    return new_std_err, new_m, new_s
-
-def _get_m_s(xs, k, old_m, old_s):
-    for i in range(k):
-        new_m = old_m + (xs[i] - old_m) / (i + 1)
-        new_s = old_s + (xs[i] - old_m) * (xs[i] - new_m)
-    return new_m, new_s
+        old_m, old_s = m, s
+    new_deltas, old_deltas = xs - ms, xs - np.insert(ms, 0, old_m, 0)[:-1]
+    new_m, new_s = ms[-1], old_s + np.sum(new_deltas * old_deltas, axis=0)
+    std_err = new_s / (n * (n - 1))
+    return std_err, new_m, new_s
