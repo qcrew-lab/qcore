65a871f3355153a10d0921e863d9cf2115ad2a28
qcrew-lab <general.qcrew@gmail.com>
2021-06-27 17:57:07 +0800

    add documentation

--- codebase/utils/fetcher.py
+++ codebase/utils/fetcher.py
@@ -8,6 +8,12 @@ from qm._results import SingleNamedJobResult, MultipleNamedJobResult
 class Fetcher:
 
     def __init__(self, handle: JobResults, num_results: int) -> None:
+        """Initialize a Fetcher instance.
+
+        Args:
+            handle (JobResults): QM job result handle
+            num_results (int): total number of results expected to be fetched
+        """
         self.total_count: int = num_results  # tota number of results to fetch
         self.count: int = 0  # current number of results fetched
         self.last_count: int = None  # last known result count
@@ -19,6 +25,8 @@ class Fetcher:
         self.is_fetching: bool = True  # to indicate fetching has started
 
     def _pre_process_results(self):
+        """Categorize the result handles based on their type, so that the appropriate QM API fetch method is called in self.fetch(). Additionally, wait for at least two values to be processed for all MultipleNamedJobResults, so that self.count and self.last_count become meaningful.
+        """
         for tag, result in self.handle:
             if isinstance(result, SingleNamedJobResult):
                 self._spec["single"][tag] = self._fetch_single
@@ -27,6 +35,11 @@ class Fetcher:
                 result.wait_for_values(2)
 
     def fetch(self) -> dict[str, np.ndarray]:
+        """To be called during a live measurement post-processing loop. Fetches the latest available results for all tags in result handle.
+
+        Returns:
+            dict[str, np.ndarray]: key -> result handle tag, value -> (1) for MultipleNamedJobResult, value is the numpy array returned by calling handle.get(tag).fetch_all(flat_struct = True). (2) For SingleNamedJobResult, value is the numpy array returned by calling handle.get(tag).fetch_all(flat_struct = True).
+        """
         self.last_count = self.count  # get and update counts
         self.count = min(len(self.handle.get(tag)) for tag in self._spec["multiple"])
 
@@ -42,8 +55,10 @@ class Fetcher:
         return partial_results
 
     def _fetch_single(self, tag):
+        """ Internal method for dealing with SingleNamedJobResult """
         return self.handle.get(tag).fetch_all(flat_struct=True)
 
     def _fetch_multiple(self, tag):
+        """ Internal method for dealing with MultipleNamedJobResult """
         slc = slice(self.last_count, self.count)
         return self.handle.get(tag).fetch(slc, flat_struct=True)
