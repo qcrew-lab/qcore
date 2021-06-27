b845e66034174afc45a52f2c6417eb00797ed73b
qcrew-lab <general.qcrew@gmail.com>
2021-06-27 18:11:52 +0800

    test v4 script with actual meas run

--- experiments/coax_test/measurements/v4/power_rabi_v4.py
+++ experiments/coax_test/measurements/v4/power_rabi_v4.py
@@ -16,7 +16,7 @@ DATAPATH = Path.cwd() / "data"
 #########################        MEASUREMENT PARAMETERS        #########################
 
 metadata = {
-    "reps": 10000,  # number of sweep repetitions
+    "reps": 20000,  # number of sweep repetitions
     "wait": 50000,  # delay between reps in ns, an integer multiple of 4 >= 16
     "start": -2.0,  # amplitude sweep range is set by start, stop, and step
     "stop": 2.0,
@@ -114,17 +114,9 @@ with DataSaver(db) as datasaver:
 
         ####################            FETCH PARTIAL RESULTS         ##################
 
-        # NOTE for Yifan about fetcher.fetch() method behaviour
-        # fetcher.fetch() returns dict "partial_results" with key = tag, value = data
-        # "partial_results" has result data for all tags defined in stream processing
-        # if tag belongs to single result, data is got by calling:
-        # handle.get(tag).fetch_all(flat_struct = True)
-        # else if tag belongs to multiple result, data is got by calling:
-        # handle.get(tag).fetch(slice(last_count, count), flat_struct=True)
-        # where last_count and count are maintained by the Fetcher
-        # you can get current number of results fetched by calling fetcher.count
+        # fetch() returns dict "partial_results" with key = tag, value = partial data
         partial_results = fetcher.fetch()
-        num_fetched_results = fetcher.count
+        num_fetched_results = fetcher.count  # get number of results fetched so far
         if not partial_results:  # empty dict return means no new results are available
             continue  # go to beginning of live post-processing loop
 
@@ -153,7 +145,7 @@ with DataSaver(db) as datasaver:
         plotter.live_plot(
             x=xs, y=ys_avg[-1], n=num_fetched_results, fit=ys_fit, err=stats[0]
         )
-        time.sleep(0.5)  # prevent over-fetching, over-saving, ulta-fast live plotting
+        time.sleep(1)  # prevent over-fetching, over-saving, ulta-fast live plotting
 
     #######################         SAVE REMAINING DATA         ########################
 
