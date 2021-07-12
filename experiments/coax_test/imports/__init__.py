7f7c0edf272b6c9452c756b90f280513f348cec8
qcrew-lab <general.qcrew@gmail.com>
2021-07-12 14:49:43 +0800

    v4 changed to fix data saving

--- experiments/coax_test/imports/__init__.py
+++ experiments/coax_test/imports/__init__.py
@@ -1,35 +1,23 @@
-from importlib import reload
-from pathlib import Path
-import time
-
+# general packages
+import matplotlib.pyplot as plt
 import numpy as np
-from qm.qua import *
+from IPython import display
+import time
+import sys
+from datetime import datetime, date
+from pathlib import Path
+from importlib.util import resolve_name
+from importlib import reload
 
-from qcrew.experiments.coax_test.imports import configuration as cfg
-from qcrew.experiments.coax_test.imports import stage as stg
-from qcrew.codebase.datasaver.hdf5_helper import initialise_database, DataSaver
+# qcrew modules
+from qcrew.codebase.analysis.plot import plot_fit
+from qcrew.codebase.analysis.qm_get_results import update_results
 from qcrew.codebase.utils.fetcher import Fetcher
 from qcrew.codebase.utils.plotter import Plotter
 from qcrew.codebase.utils.statistician import get_std_err
-
-
-################           IMPORTS NOT NEEDED FOR V4 SCRIPTS           #################
-# NOTE WE NEED TO GET RID OF THESE ASAP!
-
-import matplotlib.pyplot as plt
-from IPython import display
-import scipy
-import h5py
-from qcrew.codebase.analysis.plot import plot_fit
-from qcrew.codebase.analysis.qm_get_results import update_results
-from qcrew.codebase.analysis.plot import FakeLivePlotter
-from qcrew.codebase.analysis import fit
 from qcrew.codebase.utils.fixed_point_library import Fixed, Int
-from datetime import datetime, date, timedelta
+from qcrew.codebase.datasaver.hdf5_helper import initialise_database, DataSaver
+from qcrew.codebase.analysis import fit
 
-STAGE_NAME = "coax_test"  # this is used to save experimental data to the correct folder
-DAILY_SUBFOLDER = str(date.today())
-DATA_FOLDER_PATH = Path().resolve() / "data" / STAGE_NAME / DAILY_SUBFOLDER
-DATA_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
-
-test_var = 5
+# qua modules
+from qm.qua import *
--- experiments/coax_test/measurements/v4/power_rabi_v4.py
+++ experiments/coax_test/measurements/v4/power_rabi_v4.py
@@ -1,38 +1,51 @@
-""" Power Rabi measurement script v4 """
+""" Power Rabi measurement script v4.1 """
 #############################           IMPORTS           ##############################
-
 from qcrew.experiments.coax_test.imports import *
+from types import SimpleNamespace
 
-reload(cfg)
-reload(stg)  # reload stage and configuration before each measurement run
+stage_module_path = resolve_name(".stage", "qcrew.experiments.coax_test.imports")
+if stage_module_path not in sys.modules:
+    import qcrew.experiments.coax_test.imports.stage as stg
+else:
+    reload(stg)
+
 
 ##########################        DATA SAVING VARIABLES       ##########################
 
-SAMPLE_NAME = "coax_a"
+SAMPLE_NAME = "coax_A"
 EXP_NAME = "power_rabi"
-PROJECT_FOLDER_NAME = "coax_test"
+PROJECT_NAME = "coax_test"
 DATAPATH = Path.cwd() / "data"
 
 #########################        MEASUREMENT PARAMETERS        #########################
-
 metadata = {
-    "reps": 20000,  # number of sweep repetitions
-    "wait": 50000,  # delay between reps in ns, an integer multiple of 4 >= 16
-    "start": -1.9,  # amplitude sweep range is set by start, stop, and step
-    "stop": 1.9,
-    "step": 0.1,
-    "qubit_op": "pi",  # qubit pulse name as defined in the config
+    "reps": 500,  # number of sweep repetitions
+    "wait_time": 300000,  # delay between reps in ns, an integer multiple of 4 >= 16
+    "a_start": -1.9,  # amplitude sweep range is set by start, stop, and step
+    "a_stop": 1.9,
+    "a_step": 0.05,
+    "qubit_op": "gaussian",  # qubit pulse name as defined in the config
+    "qubit_name": stg.qubit.name,
     "rr_op": "readout",  # readout pulse name
-    "rr_op_ampx": 0.2,  # readout pulse amplitude scale factor
+    "rr_name": stg.rr.name,
+    "rr_op_ampx": 0.0175,  # readout pulse amplitude scale factor
     "fit_fn": "sine",  # name of the fit function
     "rr_lo_freq": stg.rr.lo_freq,  # frequency of the local oscillator driving rr
     "rr_int_freq": stg.rr.int_freq,  # frequency played by OPX to rr
     "qubit_lo_freq": stg.qubit.lo_freq,  # frequency of local oscillator driving qubit
     "qubit_int_freq": stg.qubit.int_freq,  # frequency played by OPX to qubit
+    "rr_integW1": "integW1",
+    "rr_integW2": "integW2",
 }
 
-x_start, x_stop, x_step = metadata["start"], metadata["stop"], metadata["step"]
-metadata["sweep_length"] = len(np.arange(x_start, x_stop + x_step / 2, x_step))
+# create a namespace and convert the metadata dictionary into the parameters under this name space
+
+a_start, a_stop, a_step = metadata["a_start"], metadata["a_stop"], metadata["a_step"]
+metadata["sweep_length"] = len(np.arange(a_start, a_stop + a_step / 2, a_step))
+mes = SimpleNamespace(**metadata)
+
+# Temporary solution, in version 5 it will be a measurement object whose properties are the parameters
+# The future measurement object will keep the same structure with the namep space
 
 ########################        QUA PROGRAM DEFINITION        ##########################
 
@@ -41,62 +54,70 @@ with program() as power_rabi:
     #####################        QUA VARIABLE DECLARATIONS        ######################
 
     n = declare(int)  # averaging loop variable
-    x = declare(fixed)  # sweep variable "x"
-    x_stream = declare_stream()  # to save "x"
-    I = declare(fixed)  # result variable "I"
-    I_stream = declare_stream()  # to save "I"
-    Q = declare(fixed)  # result variable "Q"
-    Q_stream = declare_stream()  # to save "Q"
+    a = declare(fixed)
+    I = declare(fixed)
+    Q = declare(fixed)
+
+    a_stream = declare_stream()
+    I_stream = declare_stream()
+    Q_stream = declare_stream()
 
     #######################        MEASUREMENT SEQUENCE        #########################
 
-    with for_(n, 0, n < metadata["reps"], n + 1):
-        with for_(x, x_start, x < x_stop + x_step / 2, x + x_step):
-            play(metadata["qubit_op"] * amp(x), stg.qubit.name)
-            align(stg.qubit.name, stg.rr.name)
+    with for_(n, 0, n < mes.reps, n + 1):
+        with for_(a, mes.a_start, a < mes.a_stop + mes.a_step / 2, a + mes.a_step):
+
+            play(mes.qubit_op * amp(a), mes.qubit_name)
+
+            align(mes.qubit_name, mes.rr_name)
             measure(
-                metadata["rr_op"] * amp(metadata["rr_op_ampx"]),
-                stg.rr.name,
+                mes.rr_op * amp(mes.rr_op_ampx),
+                mes.rr_name,
                 None,
-                demod.full("integW1", I),
-                demod.full("integW2", Q),
+                demod.full(mes.rr_integW1, I),
+                demod.full(mes.rr_integW2, Q),
             )
-            wait(int(metadata["wait"] // 4), stg.qubit.name)
-            save(x, x_stream)
+            wait(int(mes.wait_time // 4), mes.qubit_name)
+            save(a, a_stream)
             save(I, I_stream)
             save(Q, Q_stream)
 
     #####################        RESULT STREAM PROCESSING        #######################
 
     with stream_processing():
-        I_raw = I_stream.buffer(metadata["sweep_length"])
-        Q_raw = Q_stream.buffer(metadata["sweep_length"])  # to reshape result streams
-        I_avg = I_raw.average()
-        Q_avg = Q_raw.average()  # to get running averages
+        a_stream.buffer(mes.sweep_length).save("A")  # to save sweep variable
 
+        I_raw = I_stream.buffer(mes.sweep_length)
+        Q_raw = Q_stream.buffer(mes.sweep_length)  # to reshape result streams
+
+        I_avg = I_raw.average()
+        Q_avg = Q_raw.average()
+
+        # raw  I and Q
         I_raw.save_all("I")
-        Q_raw.save_all("Q")  # to save all raw I and Q data
+        Q_raw.save_all("Q")
 
         # we need these two streams to calculate std err in a single pass
         (I_raw * I_raw + Q_raw * Q_raw).save_all("Y_SQ_RAW")
         (I_raw * I_raw + Q_raw * Q_raw).average().save_all("Y_SQ_RAW_AVG")
+        (I_avg * I_avg + Q_avg * Q_avg).save("Y_AVG")
 
-        (I_avg * I_avg + Q_avg * Q_avg).save("Y_AVG")  # to live plot latest average
-        x_stream.buffer(metadata["sweep_length"]).save("X")  # to save sweep variable
 
 #############################        RUN MEASUREMENT        ############################
 
 job = stg.qm.execute(power_rabi)
 
 #############################        INVOKE HELPERS        #############################
-
-fetcher = Fetcher(handle=job.result_handles, num_results=metadata["reps"])
+# fetch helper and plot hepler
+fetcher = Fetcher(handle=job.result_handles, num_results=mes.reps)
 plotter = Plotter(title=EXP_NAME, xlabel="Amplitude scale factor")
 stats = (None, None, None)  # to hold running stats (stderr, mean, variance * (n-1))
+
+# initialise database under dedicated folder
 db = initialise_database(
     exp_name=EXP_NAME,
     sample_name=SAMPLE_NAME,
-    project_name=PROJECT_FOLDER_NAME,
+    project_name=PROJECT_NAME,
     path=DATAPATH,
     timesubdir=False,
     timefilename=True,
@@ -104,46 +125,40 @@ db = initialise_database(
 
 #######################        LIVE POST-PROCESSING LOOP        ########################
 
-
 with DataSaver(db) as datasaver:
 
     ####################        SAVE MEASUREMENT RUN METADATA       ####################
 
     datasaver.add_metadata(metadata)
 
-    while fetcher.is_fetching:  # while the fetcher is not done fetching all results
-
+    while fetcher.is_fetching:
         ####################            FETCH PARTIAL RESULTS         ##################
 
-        partial_results = fetcher.fetch()  # key = tag, value = partial data
-        num_results = fetcher.count  # get number of results fetched so far
-        if not partial_results:  # empty dict return means no new results are available
+        (num_so_far, update_results) = fetcher.fetch()
+        if not update_results:  # empty dict return means no new results are available
             continue
         ####################            LIVE SAVE RESULTS         ######################
-
-        # to only live save raw "I" and "Q" data, we extract them from "partial_results"
-        live_save_dict = {"I": partial_results["I"], "Q": partial_results["Q"]}
-        datasaver.update_multiple_results(live_save_dict, group="data")
+        datasaver.update_multiple_results(update_results, save=["I", "Q"], group="data")
 
         ##########            CALCULATE RUNNING MEAN STANDARD ERROR         ############
 
-        ys_raw = np.sqrt(partial_results["Y_SQ_RAW"])
-        ys_raw_avg = np.sqrt(partial_results["Y_SQ_RAW_AVG"])
-        stats = get_std_err(ys_raw, ys_raw_avg, num_results, *stats)
+        ys_raw = np.sqrt(update_results["Y_SQ_RAW"])
+        ys_raw_avg = np.sqrt(update_results["Y_SQ_RAW_AVG"])
+        stats = get_std_err(ys_raw, ys_raw_avg, num_so_far, *stats)
 
         #################            LIVE PLOT AVAILABLE RESULTS         ###############
 
-        ys = np.sqrt(partial_results["Y_AVG"])  # latest batch of average signal
-        xs = partial_results["X"]
-        plotter.live_plot(xs, ys, num_results, fit_fn=metadata["fit_fn"], err=stats[0])
+        ys = np.sqrt(update_results["Y_AVG"])  # latest batch of average signal
+        xs = update_results["A"]
+        plotter.live_plot(xs, ys, num_so_far, fit_fn=mes.fit_fn, err=stats[0])
         time.sleep(1)  # prevent over-fetching, over-saving, ulta-fast live plotting
 
     #######################         SAVE REMAINING DATA         ########################
 
-    # to save final average and sweep variables, we extract them from "partial_results"
+    # to save final average and sweep variables, we extract them from "update_results"
     final_save_dict = {"Y_AVG": ys, "X": xs}
-    datasaver.add_multiple_results(final_save_dict, group="data")
-    # NOTE (OPTIONAL) here, we can also save the final plot.
+    datasaver.add_multiple_results(final_save_dict, save=["Y_AVG", "X"], group="data")
+
 
 ###############################          fin           #################################
 
